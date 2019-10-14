dbc9ff8e57b12a235b76730610c46f60a26c2088
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-10-14 22:07:06 +0530

    File gets uploaded

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -1,15 +1,15 @@
 '''
 title           : blockchain_client.py
 description     : A blockchain client implemenation, with the following features
-                  - Wallets generation using Public/Private key encryption (based on RSA algorithm)
-                  - Generation of transactions with RSA encryption      
+				  - Wallets generation using Public/Private key encryption (based on RSA algorithm)
+				  - Generation of transactions with RSA encryption      
 author          : Adil Moujahid
 date_created    : 20180212
 date_modified   : 20180309
 version         : 0.3
 usage           : python blockchain_client.py
-                  python blockchain_client.py -p 8080
-                  python blockchain_client.py --port 8080
+				  python blockchain_client.py -p 8080
+				  python blockchain_client.py --port 8080
 python_version  : 3.6.1
 Comments        : Wallet generation and transaction signature is based on [1]
 References      : [1] https://github.com/julienr/ipynb_playground/blob/master/bitcoin/dumbcoin/dumbcoin.ipynb
@@ -31,28 +31,28 @@ from flask import Flask, jsonify, request, render_template
 
 class Transaction:
 
-    def __init__(self, sender_address, sender_private_key, recipient_address, value):
-        self.sender_address = sender_address
-        self.sender_private_key = sender_private_key
-        self.recipient_address = recipient_address
-        self.value = value
+	def __init__(self, sender_address, sender_private_key, recipient_address, value):
+		self.sender_address = sender_address
+		self.sender_private_key = sender_private_key
+		self.recipient_address = recipient_address
+		self.value = value
 
-    def __getattr__(self, attr):
-        return self.data[attr]
+	def __getattr__(self, attr):
+		return self.data[attr]
 
-    def to_dict(self):
-        return OrderedDict({'sender_address': self.sender_address,
-                            'recipient_address': self.recipient_address,
-                            'value': self.value})
+	def to_dict(self):
+		return OrderedDict({'sender_address': self.sender_address,
+							'recipient_address': self.recipient_address,
+							'value': self.value})
 
-    def sign_transaction(self):
-        """
-        Sign transaction with private key
-        """
-        private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
-        signer = PKCS1_v1_5.new(private_key)
-        h = SHA.new(str(self.to_dict()).encode('utf8'))
-        return binascii.hexlify(signer.sign(h)).decode('ascii')
+	def sign_transaction(self):
+		"""
+		Sign transaction with private key
+		"""
+		private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
+		signer = PKCS1_v1_5.new(private_key)
+		h = SHA.new(str(self.to_dict()).encode('utf8'))
+		return binascii.hexlify(signer.sign(h)).decode('ascii')
 
 
 
@@ -64,11 +64,11 @@ def index():
 
 @app.route('/make/transaction')
 def make_transaction():
-    return render_template('./make_transaction.html')
+	return render_template('./make_transaction.html')
 
 @app.route('/view/transactions')
 def view_transaction():
-    return render_template('./view_transactions.html')
+	return render_template('./view_transactions.html')
 
 @app.route('/wallet/new', methods=['GET'])
 def new_wallet():
@@ -89,6 +89,10 @@ def generate_transaction():
 	sender_private_key = request.form['sender_private_key']
 	recipient_address = request.form['recipient_address']
 	value = request.form['amount']
+	# video_file=request.form['video_file']
+	print(request.files)
+
+	
 
 	transaction = Transaction(sender_address, sender_private_key, recipient_address, value)
 
@@ -98,11 +102,11 @@ def generate_transaction():
 
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
+	from argparse import ArgumentParser
 
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=8080, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
+	parser = ArgumentParser()
+	parser.add_argument('-p', '--port', default=8080, type=int, help='port to listen on')
+	args = parser.parse_args()
+	port = args.port
 
-    app.run(host='127.0.0.1', port=port)
+	app.run(host='127.0.0.1', port=port)
